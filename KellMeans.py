25d9b843db49de6fc4959e47a1aeff65f123ee5b
HarryHeywood04 <149257196+HarryHeywood04@users.noreply.github.com>
2024-10-24 23:34:29 +0100

    Full K-Means integration

--- KellMeans.py
+++ KellMeans.py
@@ -1,6 +1,7 @@
 from operator import itemgetter
 import random
 import math
+import numpy as np
 
 
 def getMinMax(data):
@@ -14,6 +15,17 @@ def getDistance(loc1, loc2):
     return math.sqrt((diff[0] ** 2) + (diff[1] ** 2))
 
 
+def createCentroid(min_maxes):
+    return [random.randint(min_maxes[0], min_maxes[1]), random.randint(min_maxes[2], min_maxes[3]), []]
+
+
+def getLocs(centroids):
+    ret = []
+    for centroid in centroids:
+        ret.append([centroid[0], centroid[1]])
+    return ret
+
+
 class KellMeans:
     centroids = []
     data = []
@@ -25,13 +37,27 @@ class KellMeans:
         # Get data boundaries
         min_maxes = getMinMax(self.data)
         # Generate random centroids inside data boundaries
-        self.centroids.append([random.randint(min_maxes[0], min_maxes[1]), random.randint(min_maxes[2], min_maxes[3]), []])
-        self.centroids.append([random.randint(min_maxes[0], min_maxes[1]), random.randint(min_maxes[2], min_maxes[3]), []])
-        self.k_means()
+        self.centroids.append(createCentroid(min_maxes))
+        self.centroids.append(createCentroid(min_maxes))
+        full_flag = False
+        while not full_flag:
+            flag = False
+            while not flag:
+                flag = self.k_means()
+            full_flag = self.kellMeans()
+
 
     def k_means(self):
+        locs1 = getLocs(self.centroids)
         self.parentData()
         self.moveCentroids()
+        locs2 = getLocs(self.centroids)
+        dists = []
+        for i in range(0, len(locs1)):
+            dists.append(getDistance(locs1[i], locs2[i]))
+        if max(dists) < 0.0001:
+            return True
+        return False
 
     def parentData(self):
         self.labels = []
@@ -48,13 +74,15 @@ class KellMeans:
 
     def moveCentroids(self):
         for centroid in self.centroids:
-            new_pos = []
             x = []
             y = []
             for point in centroid[2]:
                 x.append(point[0])
                 y.append(point[1])
-            new_pos.append(sum(x)/len(centroid[2]))
-            new_pos.append(sum(y)/len(centroid[2]))
-            centroid[0] = new_pos[0]
-            centroid[1] = new_pos[1]
+            centroid[0] = np.mean(x)
+            centroid[1] = np.mean(y)
+
+    def kellMeans(self):
+        print("SPLIT")
+        # Split based on standard deviation and mean etc
+        return True
